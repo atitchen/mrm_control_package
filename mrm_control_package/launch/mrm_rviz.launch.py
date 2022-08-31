53f4f7831d32015cf210ddb4c09862b3086ff914
atitchen <alex.titchen@gmail.com>
2022-08-31 12:31:11 +1000

    work done to importing urdf files

---
+++ mrm_control_package/launch/mrm_rviz.launch.py
@@ -0,0 +1,59 @@
+'''
+Author: Alex Titchen
+
+Launches MRM Model in RVIZ with Joint State Publisher
+'''
+
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.substitutions import Command
+from ament_index_python.packages import get_package_share_directory
+
+
+
+def generate_launch_description():
+
+	# robot_model = "mrm_01.urdf"
+	robot_model = 'a0912'
+
+	xacro_file = get_package_share_directory('my_doosan_pkg') + '/description'+'/xacro/'+ robot_model +'.urdf.xacro'
+
+
+	robot_desc = get_package_share_directory('mrm_control_package') + '/description/urdf/'+ robot_model
+
+
+	# RViz
+	rviz_config_file = get_package_share_directory('mrm_control_package') + "/rviz/view_config.rviz"
+	rviz_node = Node(package    ='rviz2',
+					 executable ='rviz2',
+					 name       ='rviz2',
+					 output     ='log',
+					 arguments  =['-d', rviz_config_file])
+
+
+	# Robot State Publisher 
+	robot_state_publisher = Node(package='robot_state_publisher',
+								 executable='robot_state_publisher',
+								 name='robot_state_publisher',
+								 output='screen',
+								 parameters=[{'robot_description': robot_desc
+								}])
+
+
+	# # Robot State Publisher 
+	# robot_state_publisher = Node(package    ='robot_state_publisher',
+	# 							 executable ='robot_state_publisher',
+	# 							 name       ='robot_state_publisher',
+	# 							 output     ='both',
+	# 							 parameters =[{'robot_description': Command(['xacro', ' ', xacro_file])           
+	# 							}])							
+	
+	
+	# Joint State Publisher 
+	joint_state_publisher_gui = Node(package  ='joint_state_publisher_gui',
+									executable='joint_state_publisher_gui',
+									output    ='screen',
+									name      ='joint_state_publisher_gui')
+
+
+	return LaunchDescription([robot_state_publisher, joint_state_publisher_gui, rviz_node ])
---
+++ mrm_control_package/readme.md
@@ -0,0 +1,25 @@
+
+## Robot Manipulator Control Package 
+#
+
+
+## Setup Instructions
+#
+
+### ROS 2 (Humble) 
+Documentation - https://docs.ros.org/en/humble/Installation.html
+#
+
+
+
+### Gazebo and ROS2 Guide 
+Documentation - https://davidvalenciaredro.wixsite.com/my-site/services-7
+
+Repo - https://github.com/dvalenciar/robotic_arm_environment
+#
+
+
+
+### movit: 
+Documentation- https://moveit.picknik.ai/humble/doc/tutorials/getting_started/getting_started.html 
+#
